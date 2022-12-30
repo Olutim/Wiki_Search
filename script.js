22f8894b1b7483e5289a64a57cf68f785e685edf
Olutim <timolbiz5214@gmail.com>
2022-12-30 17:14:43 +0100

    A Bug Fixed

--- script.js
+++ script.js
@@ -1,24 +1,27 @@
 const searchedQuery = document.querySelector('#searchedQuery');
 const resultCollections = document.querySelector('resultCollections');
 
-searchedQuery.focus();
+searchedQuery.select();
 
 searchedQuery.addEventListener('input', function(event) {
-    console.log(event.target.value);
+    query(event.target.value);
 });
 
 // Consuming Wikipedia Search API
 const query = async (searchedQuery) => {
     try{
-        const initiateGetRequest = await fetch(`https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info|extracts&inprop=url&utf8=&format=json&origin=*&srlimit=10&srsearch=${searchedQuery}`);
-        const response = await initiateGetRequest.json();
+        const initiateGetRequest = `https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info|extracts&inprop=url&utf8=&format=json&origin=*&srlimit=10&srsearch=${searchedQuery}`;
+        const response = await fetch(initiateGetRequest);
+        const getRequest = await response.json();
         console.log({
             'term': searchedQuery,
-            'results': response.query.search
-        })
+            'results': getRequest.query.search
+        });
+
     }
     catch(error){
-        console.error(`An error must have occurred: ${error}`);
+        console.log(error);
     }
 }
 
+// query();
