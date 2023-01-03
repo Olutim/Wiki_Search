04ff2cf7e24c312353c20e43b1075c12fac586db
Olutim <timolbiz5214@>
2023-01-03 06:09:00 +0100

    Implementing Debounce Function Futhermore

--- script.js
+++ script.js
@@ -7,26 +7,10 @@ searchedQuery.addEventListener('input', function(event) {
     query(event.target.value);
 });
 
-// Consuming Wikipedia Search API
-// const query = async (searchedQuery) => {
-//     try{
-//         const response = await fetch(`https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info|extracts&inprop=url&utf8=&format=json&origin=*&srlimit=10&srsearch=${searchedQuery}`);
-//         const getRequest = await response.json();
-//         console.log({
-//             'term': searchedQuery,
-//             'results': getRequest.query.search
-//         });
-
-//     }
-//     catch(error){
-//         console.log(error);
-//     }
-// }
-
 let timerIndicator;
 
 const query = (searchedQuery) => {
-    
+
     if(timerIndicator){
         clearTimeout(timerIndicator);
     }
@@ -35,10 +19,13 @@ const query = (searchedQuery) => {
         try{
             const response = await fetch(`https://en.wikipedia.org/w/api.php?action=query&list=search&prop=info|extracts&inprop=url&utf8=&format=json&origin=*&srlimit=10&srsearch=${searchedQuery}`);
             const getRequest = await response.json();
-            console.log({
-                'term': searchedQuery,
-                'results': getRequest.query.search
-            });
+            // console.log({
+            //     'term': searchedQuery,
+            //     'results': getRequest.query.search
+            // });
+
+            const searchResulttHtml = createHTML(getRequest.query.search, searchedQuery);
+            resultCollections.innerHTML = searchResulttHtml;
 
         }
         catch(error){
@@ -46,3 +33,30 @@ const query = (searchedQuery) => {
         }
     }, 500)
 }
+
+const deHtml = (html) => {
+    let deDiv = document.createElement('div');
+    deDiv.textContent = html;
+    return deDiv.textContent;
+};
+
+const highlightSearchedTexts = (str, searchedWord, className = 'highlight') => {
+    const h1 = `<span class = '${className}'>${searchedWord}</span>`;
+    return str.replace(new RegExp(searchedWord, 'gi'), h1);
+};
+
+const createHTML = (results, searchedQuery) => {
+    return results
+        .map(result => {
+            const title = highlightSearchedTexts(deHtml(result.title), searchedQuery);
+            const snippet = highlightSearchedTexts(deHtml(result.title), searchedQuery);
+
+            return `<article>
+                 <a href="https://en.wikipedia.org/?curid=${result.pageid}">
+                    <h2>${title}</h2>
+                </a>
+                <div class="summary">${snippet}....</div>
+            </article>`;
+        })
+        .join('');
+};
